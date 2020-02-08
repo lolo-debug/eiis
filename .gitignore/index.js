01f0a9510a4f44d5e9af343cf81c483d5e4ada83
lolo-debug <60298257+lolo-debug@users.noreply.github.com>
2020-02-08 18:43:47 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js"); 
+const { Client, RichEmbed } = require('discord.js');
+const client = new Discord.Client();bot.login("Njc1NzI2NjkwNjgwMzA3NzU4.Xj7VlA.GjpeOXOvyUw9qrm39ZWriZZg_Qc");
+
+client.on('ready', async () => {
+    console.log("je suis pret")
+    client.user.setStatus('je test')
+    client.user.setPresence({
+        game: {
+            name: 'je suis le bot 7EI',
+            type: "STREAMING",
+            url: "https://www.twitch.tv/neytoux1"
+        }
+    })
+});
